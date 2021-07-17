ebe904c52e17c162a0e204a8e877d47a5b85e325
ErbolatZzz <e.karisov@yandex.ru>
2021-07-17 10:09:47 +0300

    доработка работа с числами

--- src/HWStream1/StreamMain.java
+++ src/HWStream1/StreamMain.java
@@ -1,37 +1,33 @@
 package HWStream1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
         int[] a = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
-        int[] c = new int[a.length];
-
+        List<Integer> list = new ArrayList<>();
         printArray(a);
 
         int k = 0;
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] > k && a[j] % 2 == k) {
-                    int b = a[j];
-                    c[j] = b;
-                }
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] > k && a[j] % 2 == k) {
+                list.add(a[j]);
             }
-            Arrays.sort(c);
-            printArray(c);
         }
+        Collections.sort(list);
+        System.out.println(list);
+    }
 
     public static void printArray ( int[] arr){
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
+}
 
 
 
